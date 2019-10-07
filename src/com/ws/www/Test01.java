509bc1f62ef283a671799ed1291c9cecf7794e5e
lijindong123 <2049915725@qq.com>
2019-10-07 11:57:05 +0800

    把输出的1和2变长了

--- src/com/ws/www/Test01.java
+++ src/com/ws/www/Test01.java
@@ -4,7 +4,7 @@ public class Test01 {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("我是test01111");
+		System.out.println("我是test011111111");
 		
 	}
 	
--- src/com/ws/www/Test02.java
+++ src/com/ws/www/Test02.java
@@ -4,7 +4,7 @@ public class Test02 {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("我是test02222");
+		System.out.println("我是test022222222222222");
 		
 	}
 	
