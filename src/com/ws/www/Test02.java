defe936921409adf55a41fbef9e87f3a749a3efa
lijindong123 <2049915725@qq.com>
2019-10-07 17:40:01 +0800

    test02变成4行输出了

--- src/com/ws/www/Test02.java
+++ src/com/ws/www/Test02.java
@@ -5,6 +5,9 @@ public class Test02 {
 	public static void main(String[] args) {
 		
 		System.out.println("22222222222");
+		System.out.println("22222222222");
+		System.out.println("22222222222");
+		System.out.println("22222222222");
 		
 		
 	}
