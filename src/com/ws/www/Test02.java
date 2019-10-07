fbbf874d945b3a3a89304ea599e32bc0d5af0033
lijindong123 <2049915725@qq.com>
2019-10-07 17:24:15 +0800

    删了test02的三行，自己加了一个方法

--- src/com/ws/www/Test02.java
+++ src/com/ws/www/Test02.java
@@ -4,9 +4,12 @@ public class Test02 {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("我是test022222222222222");
-		System.out.println("我是test022222222222222");
-		System.out.println("我是test022222222222222 ");
+		
+		
+		
+	}
+	
+	public void test() {
 		
 	}
 	
