ded96a29745ba445ae614661c4d7ac6579f63338
lijindong123 <2049915725@qq.com>
2019-10-07 11:40:38 +0800

    第一次创建了2个文件，只是输出了1111和22222

---
+++ src/com/ws/www/Test01.java
@@ -0,0 +1,11 @@
+package com.ws.www;
+
+public class Test01 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("我是test01111");
+		
+	}
+	
+}
---
+++ src/com/ws/www/Test02.java
@@ -0,0 +1,11 @@
+package com.ws.www;
+
+public class Test02 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("我是test02222");
+		
+	}
+	
+}
