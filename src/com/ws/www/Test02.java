834841f5f03cff4f024e2746a6851eb4bb265f92
lijindong123 <2049915725@qq.com>
2019-10-07 16:15:17 +0800

    test02变成了3行

--- src/com/ws/www/Test02.java
+++ src/com/ws/www/Test02.java
@@ -5,6 +5,8 @@ public class Test02 {
 	public static void main(String[] args) {
 		
 		System.out.println("我是test022222222222222");
+		System.out.println("我是test022222222222222");
+		System.out.println("我是test022222222222222");
 		
 	}
 	
